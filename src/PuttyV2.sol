597576194edda9f11a0d5b74c1fc27fb88219308
outdoteth <outdoteth@users.noreply.github.com>
2022-07-14 10:39:51 +0100

    add struct comments

--- src/PuttyV2.sol
+++ src/PuttyV2.sol
@@ -55,16 +55,41 @@ contract PuttyV2 is PuttyV2Nft, EIP712("Putty", "2.0"), ERC721TokenReceiver, Own
 
     using SafeTransferLib for ERC20;
 
+    /**
+        @notice ERC20 asset details.
+        @param token The token address for the erc20 asset.
+        @param tokenAmount The amount of erc20 tokens.
+     */
     struct ERC20Asset {
         address token;
         uint256 tokenAmount;
     }
 
+    /**
+        @notice ERC721 asset details.
+        @param token The token address for the erc721 asset.
+        @param tokenId The token id of the erc721 assset.
+     */
     struct ERC721Asset {
         address token;
         uint256 tokenId;
     }
-
+    /**
+        @notice Order details.
+        @param maker The maker of the order.
+        @param isCall Whether or not the order is for a call or put option.
+        @param isLong Whether or not the order is long or short.
+        @param baseAsset The erc20 contract to use for the strike and premium.
+        @param strike The strike amount.
+        @param premium The premium amount.
+        @param duration The duration of the option contract (in seconds).
+        @param expiration The timestamp after which the order is no longer (unix).
+        @param nonce A random number for each order to prevent hash collisions.
+        @param whitelist A list of addresses that are allowed to fill this order - if empty then anyone can fill.
+        @param floorTokens A list of erc721 contract addresses for the underlying.
+        @param erc20Assets A list of erc20 assets for the underlying.
+        @param erc721Assets A list of erc721 assets for the underlying.
+     */
     struct Order {
         address maker;
         bool isCall;
